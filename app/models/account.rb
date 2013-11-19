80fa409e1108a3d6a4bc7c3dcb4c5eb5b7a6b08e
aylinweiss <aylin.weiss.AW@googlemail.com>
2013-11-19 14:09:34 +0100

    3.bis aufgabe 9

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,14 @@
 class Account < ActiveRecord::Base
 	belongs_to :customer
 	has_many :transactions 
+
+	validates :number,
+						presence: true
+						uniqueness: true
+	validates :balance,
+						presence: true,
+						numericality:{greater_than_or_equal_to: 0.0}
+
+
+
 end
